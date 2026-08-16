ff17ee545201dd1306e0804f3fdb73751fdf5282
Mostafa Tacoov <mostafatacoov@gmail.com>
2026-08-16 14:18:43 +0300

    fix: force server.js to run in production mode to fix chunk 404s

--- frontend/server.js
+++ frontend/server.js
@@ -2,7 +2,8 @@ const { createServer } = require('http')
 const { parse } = require('url')
 const next = require('next')
 
-const dev = process.env.NODE_ENV !== 'production'
+// Force production mode unless explicitly set to 'development'
+const dev = process.env.NODE_ENV === 'development'
 const hostname = 'localhost'
 const port = process.env.PORT || 3000
 
